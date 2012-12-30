ef490a37b87b3f8dbe3ab1e9fc0ba87ac7bfb43c
cd01 <ibdcd01@gmail.com>
2012-12-31 00:49:28 +0900

    Rename config.py

--- mklink.bat
+++ mklink.bat
@@ -5,4 +5,4 @@ mklink    "%USERPROFILE%/.vimperatorrc"                    "%USERPROFILE%\Dropbo
 mklink /d "%USERPROFILE%/vimfiles"                         "%USERPROFILE%\Dropbox\dotfiles\.vim"
 mklink /d "%USERPROFILE%/vimperator"                       "%USERPROFILE%\Dropbox\dotfiles\.vimperator"
 mklink    "%USERPROFILE%/.minttyrc"                        "%USERPROFILE%\Dropbox\dotfiles\.minttyrc"
-mklink    "%USERPROFILE%/AppData/Roaming/keyhac/config.py" "%USERPROFILE%\Dropbox\dotfiles\config.py"
+mklink    "%USERPROFILE%/AppData/Roaming/keyhac/config.py" "%USERPROFILE%\Dropbox\dotfiles\keyhac_config.py"
