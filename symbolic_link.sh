66c1448fc95b7346b0653d2c5ccdba9dfd30709d
cd01 <ibdcd01@Gmail.com>
2013-07-19 01:11:48 +0900

    Add .tmux.conf

--- symbolic_link.sh
+++ symbolic_link.sh
@@ -6,3 +6,4 @@ ln -s ~/Dropbox/dotfiles/.vimperatorrc ~/.vimperatorrc
 ln -s ~/Dropbox/dotfiles/.vimperator ~/.vimperator
 ln -s ~/Dropbox/dotfiles/.zshrc ~/.zshrc
 ln -s ~/Dropbox/dotfiles/.bashrc ~/.bashrc
+ln -s ~/Dropbox/dotfiles/.tmux.conf ~/.tmux.conf
