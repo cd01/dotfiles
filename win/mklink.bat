07b3e32066fe0f66251f5984c67919a88033e5ca
cd01 <ibdcd01@Gmail.com>
2014-06-23 19:17:31 +0900

    Change directory name in mklink.bat

--- win/mklink.bat
+++ win/mklink.bat
@@ -1,20 +1,20 @@
 if exist "%USERPROFILE%\.bashrc" del "%USERPROFILE%\.bashrc"
-mklink /h "%USERPROFILE%\.bashrc" "%USERPROFILE%\Dropbox\dotfiles\.bashrc"
+mklink /h "%USERPROFILE%\.bashrc" "%USERPROFILE%\dotfiles\.bashrc"
 
 if exist "%USERPROFILE%\.vimrc" del "%USERPROFILE%\.vimrc"
-mklink /h "%USERPROFILE%\.vimrc" "%USERPROFILE%\Dropbox\dotfiles\.vimrc"
+mklink /h "%USERPROFILE%\.vimrc" "%USERPROFILE%\dotfiles\.vimrc"
 
 if exist "%USERPROFILE%\.gvimrc" del "%USERPROFILE%\.gvimrc"
-mklink /h "%USERPROFILE%\.gvimrc" "%USERPROFILE%\Dropbox\dotfiles\.gvimrc"
+mklink /h "%USERPROFILE%\.gvimrc" "%USERPROFILE%\dotfiles\.gvimrc"
 
 if exist "%USERPROFILE%\.vimperatorrc" del "%USERPROFILE%\.vimperatorrc"
-mklink /h "%USERPROFILE%\.vimperatorrc" "%USERPROFILE%\Dropbox\dotfiles\.vimperatorrc"
+mklink /h "%USERPROFILE%\.vimperatorrc" "%USERPROFILE%\dotfiles\.vimperatorrc"
 
 if exist "%USERPROFILE%\.vsvim" del "%USERPROFILE%\.vsvim"
-mklink /h "%USERPROFILE%\.vsvim" "%USERPROFILE%\Dropbox\dotfiles\win\.vsvim"
+mklink /h "%USERPROFILE%\.vsvim" "%USERPROFILE%\dotfiles\win\.vsvim"
 
-mklink /d "%USERPROFILE%\vimfiles"                         "%USERPROFILE%\Dropbox\dotfiles\.vim"
-mklink /d "%USERPROFILE%\.vimperator"                      "%USERPROFILE%\Dropbox\dotfiles\.vimperator"
-mklink /h "%USERPROFILE%\.minttyrc"                        "%USERPROFILE%\Dropbox\dotfiles\win\.minttyrc"
-mklink /h "%USERPROFILE%\AppData\Roaming\keyhac\config.py" "%USERPROFILE%\Dropbox\dotfiles\win\keyhac_config.py"
-mklink /h "%USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "%USERPROFILE%\Dropbox\dotfiles\win\Microsoft.PowerShell_profile.ps1"
+mklink /d "%USERPROFILE%\vimfiles"                         "%USERPROFILE%\dotfiles\.vim"
+mklink /d "%USERPROFILE%\.vimperator"                      "%USERPROFILE%\dotfiles\.vimperator"
+mklink /h "%USERPROFILE%\.minttyrc"                        "%USERPROFILE%\dotfiles\win\.minttyrc"
+mklink /h "%USERPROFILE%\AppData\Roaming\keyhac\config.py" "%USERPROFILE%\dotfiles\win\keyhac_config.py"
+mklink /h "%USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "%USERPROFILE%\dotfiles\win\Microsoft.PowerShell_profile.ps1"
